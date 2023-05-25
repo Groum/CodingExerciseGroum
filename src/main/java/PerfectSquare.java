b6ae486e65d1ff1049bc9a027b87ec8bd284f62a
groum <groums@yahoo.com>
2023-05-25 11:34:01 -0400

    number entery and perfect Square validation

--- src/main/java/PerfectSquare.java
+++ src/main/java/PerfectSquare.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class PerfectSquare {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PerfectSquare {
+    public List<Integer> ingest(List<Integer> ListCollected)
+    {
+        System.out.println("The following numbers are received and 0 " +
+                "is not accepted as entry but 0 is a Prefect Square \n");
+
+        ListCollected.stream().
+                filter(x ->x!=0)
+                .forEach(System.out :: println);
+        List<Integer> ArrayListCollectedb = checkPerfectSquare(ListCollected);
+        return ArrayListCollectedb;
+
+    }
+
+        List<Integer>  checkPerfectSquare(List<Integer> ListCollected)
+    {
+
+                 List<Integer> evenNumbers = ListCollected.stream()
+                .filter(i -> Math.sqrt(i) % 1 == 0)
+                .filter(j -> j !=0)
+                .collect(Collectors.toList());
+
+        System.out.println("The following numbers are Perfect Square from your list");
+        evenNumbers.forEach(System.out::println);
+
+        return evenNumbers;
+
+    }
+
+
+
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Runner {
+
+
+            public static void main(String[]args) {
+
+                PerfectSquare obj = new PerfectSquare();
+                int inputNumber;
+                List<Integer> collected = new ArrayList<Integer>();
+                Scanner scanIn = new Scanner(System.in);
+                System.out.println("Type the numbers to check for perfect Square\n"+
+                        " Or Type 0 to exit" );
+                do {
+                    inputNumber = scanIn.nextInt();
+
+                    collected.add(inputNumber);
+
+                } while(inputNumber != 0);
+
+                  obj.ingest(collected);
+
+           }
+
 }
